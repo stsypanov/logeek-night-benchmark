e180534f648b6db3644692abb039ea4325b3ed19
stsypanov <sergei.tsypanov@yandex.ru>
2017-02-15 21:45:24 +0200

    import JpaConfig

--- src/main/java/com/luxoft/logeek/AppConfig.java
+++ src/main/java/com/luxoft/logeek/AppConfig.java
@@ -2,8 +2,10 @@ package com.luxoft.logeek;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 @Configuration
+@Import(JpaConfig.class)
 @ComponentScan(basePackages = {"com.luxoft.logeek"})
 public class AppConfig {
 	
