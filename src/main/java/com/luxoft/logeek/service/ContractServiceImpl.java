256aed1d2ff5a3a84ec518caa61b996c7c667847
stsypanov <sergei.tsypanov@yandex.ru>
2017-02-25 16:00:38 +0200

    rename

--- src/main/java/com/luxoft/logeek/service/ContractServiceImpl.java
+++ src/main/java/com/luxoft/logeek/service/ContractServiceImpl.java
@@ -10,57 +10,67 @@ import java.util.function.Predicate;
 @Component
 public class ContractServiceImpl implements ContractService {
 
-	private LoanContractRepository loanContractRepository;
+	private LoanContractRepository jpaRepository;
 
 	@Autowired
-	public ContractServiceImpl(LoanContractRepository loanContractRepository) {
-		this.loanContractRepository = loanContractRepository;
+	public ContractServiceImpl(LoanContractRepository jpaRepository) {
+		this.jpaRepository = jpaRepository;
 	}
 
 	@Override
 	public long doCalculation(Long facility) {
-		BigDecimal compensation = loanContractRepository.findCompensationByFacility(facility);
-		BigDecimal deposit = loanContractRepository.findDepositByFacilityId(facility);
-		BigDecimal outstanding = loanContractRepository.findOutstandingByFacility(facility);
+		BigDecimal compensation = jpaRepository.findCompensationByFacility(facility);
+		BigDecimal deposit = jpaRepository.findDepositByFacilityId(facility);
+		BigDecimal outstanding = jpaRepository.findOutstandingByFacility(facility);
 
 		boolean hasCompensation = compensation.signum() > 0;
 		boolean hasDeposit = deposit.signum() > 0;
 		boolean hasOutstanding = outstanding.signum() > 0;
 
 		if (hasDeposit || hasCompensation) {
-			return System.currentTimeMillis();
+			return calculateWihtCompensation();
 		} else if (hasOutstanding) {
-			return System.currentTimeMillis();
+			return calculateWihtOutstanding();
 		} else {
-			return System.currentTimeMillis();
+			return calculateDefault();
 		}
 	}
 
-
 	private final Predicate<Long> hasCompensation = facility -> {
-		BigDecimal compensation = loanContractRepository.findCompensationByFacility(facility);
+		BigDecimal compensation = jpaRepository.findCompensationByFacility(facility);
 		return compensation.signum() > 0;
 	};
 
 	private final Predicate<Long> hasDeposit = facility -> {
-		BigDecimal deposit = loanContractRepository.findDepositByFacilityId(facility);
+		BigDecimal deposit = jpaRepository.findDepositByFacilityId(facility);
 		return deposit.signum() > 0;
 	};
 
 	private final Predicate<Long> hasOutstanding = facility -> {
-		BigDecimal outstanding = loanContractRepository.findOutstandingByFacility(facility);
+		BigDecimal outstanding = jpaRepository.findOutstandingByFacility(facility);
 		return outstanding.signum() > 0;
 	};
 
 	@Override
 	public long doCalculationLazily(Long facility) {
 		if (hasDeposit.or(hasCompensation).test(facility)) {
-			return System.currentTimeMillis() * 2 - 5;
+			return calculateWihtCompensation();
 		} else if (hasOutstanding.test(facility)) {
-			return System.currentTimeMillis() * 3 - 6;
+			return calculateWihtOutstanding();
 		} else {
-			return System.currentTimeMillis() * 4 - 7;
+			return calculateDefault();
 		}
 	}
 
+	private long calculateWihtCompensation() {
+		return System.currentTimeMillis() << 2;
+	}
+
+	private long calculateWihtOutstanding() {
+		return System.currentTimeMillis() >> 6;
+	}
+
+	private long calculateDefault() {
+		return System.currentTimeMillis() >>> 3;
+	}
 }
