9c4f0acf0e30af077ce7f6db8bfd9a7e1a34f179
stsypanov <STsypanov@luxoft.com>
2017-02-27 17:34:34 +0200

    remove uselesss

--- src/main/java/com/luxoft/logeek/factory/LtavActualizerFactory.java
+++
@@ -1,8 +0,0 @@
-package com.luxoft.logeek.factory;
-
-import com.luxoft.logeek.utils.DatasetStatus;
-import com.luxoft.logeek.utils.LtavActualizer;
-
-public interface LtavActualizerFactory {
-	LtavActualizer forDataset(String extId, DatasetStatus datasetStatus);
-}
