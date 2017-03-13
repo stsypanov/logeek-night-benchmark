3d91d678833f22db9b93d0fd25fd2ae3de6e96b7
stsypanov <sergei.tsypanov@yandex.ru>
2017-03-13 22:04:11 +0200

    add SomeData.java

---
+++ src/main/java/com/luxoft/logeek/dto/SomeData.java
@@ -0,0 +1,17 @@
+package com.luxoft.logeek.dto;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+public class SomeData {
+    private long id;
+    private String someName;
+    private String anotherString;
+    private List someChildProperties;
+    private int count;
+
+
+}
