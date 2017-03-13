6d4f78c41c7f814e2e8f17dc256d55dc76856a4b
stsypanov <sergei.tsypanov@yandex.ru>
2017-03-13 22:27:45 +0200

    ass some more fields

--- src/main/java/com/luxoft/logeek/dto/SomeData.java
+++ src/main/java/com/luxoft/logeek/dto/SomeData.java
@@ -4,14 +4,17 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDate;
+import java.util.Date;
 import java.util.List;
 
 public class SomeData {
     private long id;
     private String someName;
-    private String anotherString;
     private List someChildProperties;
+    private String anotherString;
     private int count;
-
+    private LocalDate localDate;
+    private Date date;
 
 }
