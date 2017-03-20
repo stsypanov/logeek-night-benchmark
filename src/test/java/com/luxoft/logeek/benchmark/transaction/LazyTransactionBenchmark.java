a55e8238a9de316d9ab322d9d38bb204ad21de88
stsypanov <STsypanov@luxoft.com>
2017-03-20 18:15:28 +0200

    merge two benchmarks into one class

--- src/test/java/com/luxoft/logeek/benchmark/transaction/LazyTransactionBenchmark.java
+++ src/test/java/com/luxoft/logeek/benchmark/transaction/LazyTransactionBenchmark.java
@@ -1,36 +1,60 @@
 package com.luxoft.logeek.benchmark.transaction;
 
+import com.luxoft.logeek.benchmark.BenchmarkBase;
 import com.luxoft.logeek.dto.CashFlowDTO;
+import com.luxoft.logeek.service.ltav.EagerLtavService;
 import com.luxoft.logeek.service.ltav.LazyLtavService;
 import org.openjdk.jmh.annotations.*;
 
 import java.util.concurrent.TimeUnit;
 
-@BenchmarkMode(Mode.AverageTime)
+@BenchmarkMode({Mode.AverageTime, Mode.Throughput})
 @OutputTimeUnit(TimeUnit.MICROSECONDS)
 @State(Scope.Benchmark)
-public class LazyTransactionBenchmark extends TransactionBenchmark {
+public class LazyTransactionBenchmark extends BenchmarkBase {
 	private CashFlowDTO dto;
-	private LazyLtavService service;
+	private LazyLtavService lazyService;
+	private EagerLtavService eagerService;
 
 	@Setup()
 	public void init() {
 		super.initContext();
-		service = context.getBean(LazyLtavService.class);
+		lazyService = context.getBean(LazyLtavService.class);
+		eagerService = context.getBean(EagerLtavService.class);
 	}
 
 	@Setup(Level.Iteration)
 	public void prepareFreshData() {
-		dto = super.initDto();
+		dto = initDto();
 	}
 
 	@Benchmark
-	public Long execute() {
+	public Long measureLazyTransaction() {
 		try {
-			return service.createLtavCashFlow(dto);
+			return lazyService.createLtavCashFlow(dto);
 		} catch (RuntimeException e) {
 			return System.currentTimeMillis() >>> 3;
 		}
 	}
 
+	@Benchmark
+	public Long measureEagerTransaction() {
+		try {
+			return eagerService.createLtavCashFlow(dto);
+		} catch (RuntimeException e) {
+			return System.currentTimeMillis() << 2;
+		}
+	}
+
+	private CashFlowDTO initDto() {
+		CashFlowDTO dto;
+		for (int i = random.nextInt(); ; i++) {
+			if (i % 2 != 0) {
+				dto = new CashFlowDTO(i, random.nextLong());
+				break;
+			}
+		}
+
+		return dto;
+	}
 }
