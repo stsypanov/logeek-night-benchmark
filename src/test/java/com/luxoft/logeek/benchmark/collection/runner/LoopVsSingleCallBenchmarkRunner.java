8553a66a0056df27900822c556a453b9c90fce5f
stsypanov <STsypanov@luxoft.com>
2017-03-20 18:06:40 +0200

    merge two benchmarks into one class

--- src/test/java/com/luxoft/logeek/benchmark/collection/runner/LoopVsSingleCallBenchmarkRunner.java
+++ src/test/java/com/luxoft/logeek/benchmark/collection/runner/LoopVsSingleCallBenchmarkRunner.java
@@ -1,22 +1,21 @@
 package com.luxoft.logeek.benchmark.collection.runner;
 
-import com.luxoft.logeek.benchmark.collection.LoopBenchmark;
+import com.luxoft.logeek.benchmark.collection.LoopVsSingleCallBenchmark;
 import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.RunnerException;
 import org.openjdk.jmh.runner.options.Options;
 import org.openjdk.jmh.runner.options.OptionsBuilder;
 
-public class LoopBenchmarkRunner {
-	
+public class LoopVsSingleCallBenchmarkRunner {
+
 	public static void main(String[] args) throws RunnerException {
 		Options opt = new OptionsBuilder()
-				.include(LoopBenchmark.class.getSimpleName())
+				.include(LoopVsSingleCallBenchmark.class.getSimpleName())
 				.warmupIterations(5)
 				.measurementIterations(20)
 				.forks(1)
 				.build();
 
 		new Runner(opt).run();
-
 	}
 }
