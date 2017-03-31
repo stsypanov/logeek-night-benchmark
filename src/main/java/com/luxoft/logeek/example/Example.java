3e4a1793658b9c1910ccd3358cc04ccd853da3eb
stsypanov <sergei.tsypanov@yandex.ru>
2017-03-31 22:03:25 +0300

    add example

---
+++ src/main/java/com/luxoft/logeek/example/Example.java
@@ -0,0 +1,10 @@
+package com.luxoft.logeek.example;
+
+import java.text.SimpleDateFormat;
+
+public class Example {
+	public static void main(String[] args) {
+		SimpleDateFormat dateFormat = new SimpleDateFormat("dd.mm.yyyy");
+		
+	}
+}
