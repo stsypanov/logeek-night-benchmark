1067ed3646a7d344af5d0d26a00f0e0be4e0ff36
stsypanov <sergei.tsypanov@yandex.ru>
2017-06-15 19:20:57 +0300

    add FormatterBenchmark

---
+++ src/test/java/com/luxoft/logeek/benchmark/formatter/FormatterBenchmark.java
@@ -0,0 +1,27 @@
+package com.luxoft.logeek.benchmark.formatter;
+
+import com.luxoft.logeek.benchmark.BenchmarkBase;
+import org.openjdk.jmh.annotations.*;
+
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+@BenchmarkMode({Mode.AverageTime, Mode.Throughput})
+//@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
+@State(Scope.Benchmark)
+public class FormatterBenchmark extends BenchmarkBase {
+
+    @Benchmark
+    public String measureSimpleDateTimeFormatter() {
+        return new SimpleDateFormat("dd.MM.yyyy").format(new Date());
+    }
+
+    @Benchmark
+    public String measureDateTimeFormatter() {
+        return DateTimeFormatter.ofPattern("dd.MM.yyyy").format(LocalDate.now());
+    }
+}
