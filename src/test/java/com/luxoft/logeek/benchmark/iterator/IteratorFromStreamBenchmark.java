c3a7c388261f1c7b3ccd510d77b8a252dbd9134c
stsypanov <STsypanov@luxoft.com>
2017-06-20 09:12:53 +0300

    add set iterator benchmark

--- src/test/java/com/luxoft/logeek/benchmark/iterator/IteratorFromStreamBenchmark.java
+++ src/test/java/com/luxoft/logeek/benchmark/iterator/IteratorFromStreamBenchmark.java
@@ -4,18 +4,19 @@ import com.luxoft.logeek.benchmark.BenchmarkBase;
 import org.openjdk.jmh.annotations.*;
 import org.openjdk.jmh.infra.Blackhole;
 
-import java.util.Iterator;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 import java.util.concurrent.TimeUnit;
-import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.toList;
+import static java.util.stream.Collectors.toSet;
 
 @BenchmarkMode(Mode.AverageTime)
 @OutputTimeUnit(TimeUnit.MICROSECONDS)
 @State(Scope.Benchmark)
 public class IteratorFromStreamBenchmark extends BenchmarkBase {
 
-    private List<Integer> items;
+    private List<Integer> itemList;
+    private Set<Integer> itemSet;
 
     @Param({"10", "100", "1000", "10000", "100000"})
     private int size;
@@ -23,16 +24,16 @@ public class IteratorFromStreamBenchmark extends BenchmarkBase {
     @Setup(value = Level.Iteration)
     public void init() {
         super.init();
-        items = new Random().ints(size).boxed().collect(Collectors.toList());
+        itemList = new Random().ints(size).boxed().collect(toList());
+        itemSet = new HashSet<>(itemList);
     }
 
 
     @Benchmark
-    public void measureIteratorFromListCollectedFormStream(Blackhole bh) {
-
-        Iterator<String> iterator = items.stream()
+    public void measureIteratorFromCollectedList(Blackhole bh) {
+        Iterator<String> iterator = itemList.stream()
                 .map(Object::toString)
-                .collect(Collectors.toList())
+                .collect(toList())
                 .iterator();
 
         while (iterator.hasNext())
@@ -41,13 +42,32 @@ public class IteratorFromStreamBenchmark extends BenchmarkBase {
 
     @Benchmark
     public void measureIteratorFromStream(Blackhole bh) {
-
-        Iterator<String> iterator = items.stream()
+        Iterator<String> iterator = itemList.stream()
                 .map(Object::toString)
                 .iterator();
 
+        while (iterator.hasNext())
+            bh.consume(iterator.next());
+    }
 
+    @Benchmark
+    public void measureIteratorFromHashSet(Blackhole bh) {
+        Iterator<String> iterator = itemSet
+                .stream()
+                .map(Object::toString)
+                .collect(toSet())
+                .iterator();
+        
+        while (iterator.hasNext())
+            bh.consume(iterator.next());
+    }
 
+    @Benchmark
+    public void measureIteratorFromSetStream(Blackhole bh) {
+        Iterator<String> iterator = itemSet
+                .stream()
+                .map(Object::toString)
+                .iterator();
 
         while (iterator.hasNext())
             bh.consume(iterator.next());
--- src/test/java/com/luxoft/logeek/benchmark/iterator/IteratorTest.java
+++ src/test/java/com/luxoft/logeek/benchmark/iterator/IteratorTest.java
@@ -1,21 +1,30 @@
 package com.luxoft.logeek.benchmark.iterator;
 
+import org.junit.Before;
 import org.junit.Test;
 
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Random;
-import java.util.stream.Collectors;
 
+import static java.util.stream.Collectors.toList;
+import static java.util.stream.Collectors.toSet;
 import static org.junit.Assert.assertEquals;
 
 public class IteratorTest {
 
+    private List<Integer> items;
+
+    @Before
+    public void setUp() throws Exception {
+        items = new Random().ints(100).boxed().collect(toList());
+    }
+
     @Test
     public void equals() throws Exception {
-        List<Integer> items = new Random().ints(100).boxed().collect(Collectors.toList());
 
-        Iterator<String> iterator1 = items.stream().map(Object::toString).collect(Collectors.toList()).iterator();
+        Iterator<String> iterator1 = items.stream().map(Object::toString).collect(toList()).iterator();
 
         Iterator<String> iterator2 = items.stream().map(Object::toString).iterator();
 
@@ -24,4 +33,21 @@ public class IteratorTest {
         }
     }
 
+    @Test
+    public void setEquals() throws Exception {
+        Iterator<String> iterator1 = new HashSet<>(items)
+                .stream()
+                .map(Object::toString)
+                .collect(toSet())
+                .iterator();
+
+        Iterator<String> iterator2 = new HashSet<>(items)
+                .stream()
+                .map(Object::toString)
+                .iterator();
+
+        while (iterator1.hasNext() && iterator2.hasNext()) {
+            assertEquals(iterator1.next(), iterator2.next());
+        }
+    }
 }
