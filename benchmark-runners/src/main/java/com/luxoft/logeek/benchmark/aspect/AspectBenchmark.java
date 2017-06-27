5bf82f2318d1fdca0fd85afa3fa22c7d2e8319c2
stsypanov <STsypanov@luxoft.com>
2017-06-27 17:07:27 +0300

    split into two modules to make JMH code independent

---
+++ benchmark-runners/src/main/java/com/luxoft/logeek/benchmark/aspect/AspectBenchmark.java
@@ -0,0 +1,25 @@
+package com.luxoft.logeek.benchmark.aspect;
+
+import com.luxoft.logeek.benchmark.BenchmarkWithAppContext;
+import com.luxoft.logeek.service.AspectedService;
+import org.openjdk.jmh.annotations.*;
+
+import java.util.concurrent.TimeUnit;
+
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.MICROSECONDS)
+@State(Scope.Benchmark)
+public class AspectBenchmark extends BenchmarkWithAppContext {
+	private AspectedService aspectedService;
+
+	@Setup()
+	public void init() {
+		super.init();
+		aspectedService = getBean(AspectedService.class);
+	}
+
+	@Benchmark
+	public Long execute() {
+		return aspectedService.getRandomLong();
+	}
+}
