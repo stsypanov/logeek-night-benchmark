dcf95bd2ed6bc089737f4f85ec72c5720a2965e1
stsypanov <sergei.tsypanov@yandex.ru>
2017-08-18 00:12:51 +0300

    unify 'remove-from-list' benchmarks

--- benchmark-runners/src/main/java/com/luxoft/logeek/benchmark/bulk/remove/RemoveFromArrayListBenchmark.java
+++ benchmark-runners/src/main/java/com/luxoft/logeek/benchmark/bulk/remove/RemoveFromArrayListBenchmark.java
@@ -1,45 +1,40 @@
 package com.luxoft.logeek.benchmark.bulk.remove;
 
-import com.luxoft.logeek.benchmark.BenchmarkBase;
 import org.openjdk.jmh.annotations.*;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.LongStream;
 
 import static java.util.stream.Collectors.toCollection;
 
-@Warmup(batchSize = 1000)
-@Measurement(batchSize = 1000)
+@State(Scope.Thread)
+@Warmup(batchSize = 1000, iterations = 50)
+@Measurement(batchSize = 1000, iterations = 50)
 @BenchmarkMode(Mode.SingleShotTime)
-@OutputTimeUnit(TimeUnit.MILLISECONDS)
-public class RemoveFromArrayListBenchmark extends BenchmarkBase {
+@OutputTimeUnit(TimeUnit.MICROSECONDS)
+public class RemoveFromArrayListBenchmark {
 
 	@Param({"100", "1000", "10000"})
 	private int itemsCount; 			//items count
 
-	@Param({"5", "10", "25"})
+	@Param({"5", "10", "25", "50"})
 	private int percentOfRemovedItems;  //count of items removed from list
 
 	private List<Long> arrayList;
 	private int from;
 	private int to;
 
-	@Setup
-	public void init() {
-		super.init();
-	}
-
 	@Setup(Level.Iteration)
 	public void initIteration() {
 		from = itemsCount / 2; //remove from the second half of the list
 		to = from + (itemsCount / 100 * percentOfRemovedItems);
-//		System.out.println(String.format("Items count = %d, removing items from = %d to = %d", itemsCount, from, to));
 	}
 
 	@Setup(Level.Invocation)
 	public void initInvocation() {
-		arrayList = random.longs(itemsCount).boxed().collect(toCollection(ArrayList::new));
+		arrayList = LongStream.range(0, itemsCount).boxed().collect(toCollection(ArrayList::new));
 	}
 
 	@Benchmark
