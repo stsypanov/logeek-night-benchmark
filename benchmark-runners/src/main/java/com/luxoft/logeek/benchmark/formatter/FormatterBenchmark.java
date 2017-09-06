af02aff47587483ec4653198a0d2b4aaed56ead1
stsypanov <STsypanov@luxoft.com>
2017-09-06 12:56:46 +0300

    imrove FormatterBenchmark accuracy

--- benchmark-runners/src/main/java/com/luxoft/logeek/benchmark/formatter/FormatterBenchmark.java
+++ benchmark-runners/src/main/java/com/luxoft/logeek/benchmark/formatter/FormatterBenchmark.java
@@ -2,6 +2,8 @@ package com.luxoft.logeek.benchmark.formatter;
 
 import com.luxoft.logeek.benchmark.BenchmarkBase;
 import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.Level;
+import org.openjdk.jmh.annotations.Setup;
 
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
@@ -11,19 +13,31 @@ import java.util.Date;
 
 public class FormatterBenchmark extends BenchmarkBase {
 
+    private static final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final ThreadLocal<SimpleDateFormat> simpleDateFormat = ThreadLocal.withInitial(() -> new SimpleDateFormat("dd.MM.yyyy"));
+    
+    private Date date;
+    private LocalDate localDate;
+    
+    @Setup(Level.Iteration)
+    public void setup() {
+        date = new Date();
+        localDate = LocalDate.now();
+    }
+
     @Benchmark
     public String measureSimpleDateTimeFormatter() {
-        return new SimpleDateFormat("dd.MM.yyyy").format(new Date());
+        return simpleDateFormat.get().format(date);
     }
 
     @Benchmark
     public String measureDateTimeFormatter() {
-        return DateTimeFormatter.ofPattern("dd.MM.yyyy").format(LocalDate.now());
+        return dateTimeFormatter.format(localDate);
     }
 
     @Benchmark
     public String measureDateTimeFormatterWhenDateConverted() {
-        LocalDate localDate = new Date().toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
-        return DateTimeFormatter.ofPattern("dd.MM.yyyy").format(localDate);
+        LocalDate localDate = date.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+        return dateTimeFormatter.format(localDate);
     }
 }
