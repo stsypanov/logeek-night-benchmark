1d84cd46771f79e78e1507eafca7da06a2182d1d
stsypanov <STsypanov@luxoft.com>
2017-10-13 17:33:08 +0300

    imrpove RegressionSortingBenchmark

--- benchmark-runners/src/main/java/com/luxoft/logeek/benchmark/sort/RegressionSortingBenchmark.java
+++ benchmark-runners/src/main/java/com/luxoft/logeek/benchmark/sort/RegressionSortingBenchmark.java
@@ -1,66 +1,65 @@
 package com.luxoft.logeek.benchmark.sort;
 
 import com.luxoft.logeek.benchmark.BaseBenchmark;
-import com.luxoft.logeek.sort.ArraysStub;
+import com.luxoft.logeek.sort.ComparableTimSort;
 import org.openjdk.jmh.annotations.*;
 
-import java.util.Arrays;
-import java.util.Iterator;
+import java.util.Comparator;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.LongStream;
 
 /**
  * We need this benchmark to check whether performance is not degradated for 2 cases
  * 1) array.length=2
  * 1) array.length>2
  */
-@State(Scope.Thread)
-@BenchmarkMode(Mode.Throughput)
-@OutputTimeUnit(TimeUnit.MILLISECONDS)
+@Fork(5)
+@State(Scope.Benchmark)
+@Warmup(iterations = 10)
+@Measurement(iterations = 10)
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
 public class RegressionSortingBenchmark extends BaseBenchmark {
 
-	@Param({"10", "100", "1000"})
-	private int arrayLength;
+    @Param({"5", "10", "100"})
+    private int arrayLength;
 
-	private Long[] sortedArray;
-	private Long[] notSortedInitialArray;
+    private Long[] array1;
+    private Long[] array2;
+    private Long[] array3;
 
-	@Setup
-	public void createNewArray() {
-		super.init();
-		sortedArray = random.longs(Long.MIN_VALUE, Long.MAX_VALUE)
-				.limit(arrayLength)
-				.boxed()
-				.toArray(value -> new Long[arrayLength]);
-		notSortedInitialArray = sortedArray.clone();
-	}
+    @Setup
+    public void setup() {
+        super.init();
+    }
 
-	@TearDown(Level.Invocation)
-	public void afterEachInvocation() {
-		Iterator<Long> iterator = Arrays.stream(sortedArray).iterator();
-		while (iterator.hasNext()) {
-			Long current = iterator.next();
-			if (iterator.hasNext()) {
-				Long next = iterator.next();
-				if (current.compareTo(next) > 0) {
-					//log arrays if something goes wrong
-					System.out.println("Initial array: " + Arrays.toString(notSortedInitialArray));
-					System.out.println("Sorted array: " + Arrays.toString(sortedArray));
-					throw new RuntimeException("Array must be sorted but current is greater than next: " + current + ", " + next);
-				}
-			}
-		}
-	}
+    @Setup(Level.Invocation)
+    public void createNewArray() {
+        Long[] reversedArray = LongStream.range(0, arrayLength)
+                .boxed()
+                .sorted(Comparator.reverseOrder())
+                .toArray(value -> new Long[arrayLength]);
+        array1 = reversedArray.clone();
+        array2 = reversedArray.clone();
+        array3 = reversedArray.clone();
+    }
 
-	@Benchmark
-	public Long[] measureEnhancedSort_arrayLengthGreaterThanTwo() {
-		ArraysStub.sort(sortedArray);
-		return sortedArray;
-	}
+    @Benchmark
+    public Long[] measureEnhancedSort() {
+        ComparableTimSort.sort(array1, 0, array1.length, null, 0, 0);
+        return array1;
+    }
 
-	@Benchmark
-	public Long[] measureConventionalSort_arrayLengthGreaterThanTwo() {
-		Arrays.sort(sortedArray);
-		return sortedArray;
-	}
+    @Benchmark
+    public Long[] _measureEnhancedSort() {
+        ComparableTimSort._sort(array2, 0, array2.length, null, 0, 0);
+        return array2;
+    }
+
+    @Benchmark
+    public Long[] measureConventionalSort() {
+        ComparableTimSort.sort(array3, 0, array3.length, null, 0, 0);
+        return array3;
+    }
 
 }
