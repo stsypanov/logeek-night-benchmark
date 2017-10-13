158e44f4179acd8b739a1a567c1461989b11a836
stsypanov <STsypanov@luxoft.com>
2017-10-13 10:04:19 +0300

    remove unused param

--- benchmark-source/src/main/java/com/luxoft/logeek/entity/BaseAuditEntity.java
+++ benchmark-source/src/main/java/com/luxoft/logeek/entity/BaseAuditEntity.java
@@ -2,40 +2,43 @@ package com.luxoft.logeek.entity;
 
 import java.util.*;
 
+import static com.luxoft.logeek.entity.BaseAuditEntity.ModifiedColumn.Draft.NEW_DRAFT;
+import static com.luxoft.logeek.entity.BaseAuditEntity.ModifiedColumn.Draft.OLD_DRAFT;
+
 public abstract class BaseAuditEntity {
-	protected List<ModifiedColumn> modifiedColumns = new ArrayList<>();
-	
-	public void includeInAudit(String columnName, Object oldValue, Object newValue, AuditMode... modes) {
-		Set<ModifiedColumn.Draft> drafts = new HashSet<>();
-		
-		for(AuditMode mode : modes) {
-			switch (mode) {
-				case OLD_DRAFT:
-					drafts.add(ModifiedColumn.Draft.OLD_DRAFT);
-					break;
-				case NEW_DRAFT:
-					drafts.add(ModifiedColumn.Draft.NEW_DRAFT);
-			}
-		}
-		
-		modifiedColumns.add(new ModifiedColumn(drafts));
-	}
-	
-	public static class ModifiedColumn {
-		private Set<Draft> drafts;
+    protected List<ModifiedColumn> modifiedColumns = new ArrayList<>();
 
-		public ModifiedColumn(Set<Draft> drafts) {
-			this.drafts = drafts;
-		}
+    public void includeInAudit(Object oldValue, Object newValue, AuditMode... modes) {
+        Set<ModifiedColumn.Draft> drafts = new HashSet<>();
 
-		public enum Draft {
-			OLD_DRAFT,
-			NEW_DRAFT
-		}
-	}
-	
-	public enum AuditMode {
-		OLD_DRAFT,
-		NEW_DRAFT
-	}
+        for (AuditMode mode : modes) {
+            switch (mode) {
+                case OLD_DRAFT:
+                    drafts.add(OLD_DRAFT);
+                    break;
+                case NEW_DRAFT:
+                    drafts.add(NEW_DRAFT);
+            }
+        }
+
+        modifiedColumns.add(new ModifiedColumn(drafts));
+    }
+
+    public static class ModifiedColumn {
+        private Set<Draft> drafts;
+
+        public ModifiedColumn(Set<Draft> drafts) {
+            this.drafts = drafts;
+        }
+
+        public enum Draft {
+            OLD_DRAFT,
+            NEW_DRAFT
+        }
+    }
+
+    public enum AuditMode {
+        OLD_DRAFT,
+        NEW_DRAFT
+    }
 }
