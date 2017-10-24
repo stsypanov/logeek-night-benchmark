b833597327e906fe79b8d1c43eb535b65c0d4abf
stsypanov <STsypanov@luxoft.com>
2017-10-24 16:42:26 +0300

    unify sorting benchmark

--- benchmark-runners/src/main/java/com/luxoft/logeek/benchmark/sort/SortingBenchmark.java
+++ benchmark-runners/src/main/java/com/luxoft/logeek/benchmark/sort/SortingBenchmark.java
@@ -1,57 +1,62 @@
 package com.luxoft.logeek.benchmark.sort;
 
-import com.luxoft.logeek.benchmark.BaseBenchmark;
 import com.luxoft.logeek.sort.ComparableTimSort;
 import org.openjdk.jmh.annotations.*;
 
+import java.util.Arrays;
 import java.util.concurrent.TimeUnit;
 
 @Fork(jvmArgsAppend = {"-XX:+UseParallelGC", "-Xms2g", "-Xmx2g"})
-@State(Scope.Benchmark)
 @BenchmarkMode(Mode.AverageTime)
 @OutputTimeUnit(TimeUnit.NANOSECONDS)
-public class SortingBenchmark extends BaseBenchmark {
-
-    private Long[] longs;
-
-    @Setup
-    public void setup() {
-        super.init();
-    }
-
-    @Setup(Level.Iteration)
-    public void createNewArray() {
-        long positive = Math.abs(random.nextLong());
-        long negative = random.nextLong();
-        negative = negative < 0 ? negative : negative * -1;
-        longs = new Long[]{positive, negative};
-    }
-
-    @TearDown(Level.Iteration)
-    public void afterEachInvocation() {
-        Long long0 = longs[0];
-        Long long1 = longs[1];
-        if (long0 > long1) {
-            throw new RuntimeException("Array must be sorted but was: " + long1 + ", " + long1);
-        }
-    }
+public class SortingBenchmark {
 
     @Benchmark
-    public Long[] measureEnhancedSortWithoutComparator() {
+    public Long[] measureEnhancedSort(DataCopy data) {
+        Long[] longs = data.copy;
         ComparableTimSort.sort(longs, 0, longs.length, null, 0, 0);
         return longs;
     }
 
     @Benchmark
-    public Long[] _measureEnhancedSortWithoutComparator() {
+    public Long[] _measureEnhancedSort(DataCopy data) {
+        Long[] longs = data.copy;
         ComparableTimSort._sort(longs, 0, longs.length, null, 0, 0);
         return longs;
     }
 
     @Benchmark
-    public Long[] measureConventionalSortWithoutComparator() {
+    public Long[] measureConventionalSort(DataCopy data) {
+        Long[] longs = data.copy;
         ComparableTimSort.sortOriginal(longs, 0, longs.length, null, 0, 0);
         return longs;
+    }
+
+    @State(Scope.Benchmark)
+    public static class Data {
+
+        @Param({"1", "2", "3"})
+        int count;
+
+        Long[] arr;
+
+        @Setup
+        public void setup() {
+            arr = new Long[count];
+            for (int i = 1; i <= count; i++) {
+                arr[i-1] = i * -1L;
+            }
+        }
+    }
+
+    @State(Scope.Thread)
+    public static class DataCopy {
+        Long[] copy;
+
+        @Setup(Level.Invocation)
+        public void setup(Data data) {
+            copy = Arrays.copyOf(data.arr, data.arr.length);
+        }
     }
 }
 
