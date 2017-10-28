8391b901b2cccb9e05c7db9ac964bafd339a345c
stsypanov <STsypanov@luxoft.com>
2017-10-28 15:43:49 +0300

    don't create new data for each invocation

--- benchmark-runners/src/main/java/com/luxoft/logeek/benchmark/hashcode/calcapproach/HashCodeCalculationMapBenchmark.java
+++ benchmark-runners/src/main/java/com/luxoft/logeek/benchmark/hashcode/calcapproach/HashCodeCalculationMapBenchmark.java
@@ -5,58 +5,53 @@ import com.luxoft.logeek.hashcode.calcapproach.DataClass;
 import com.luxoft.logeek.hashcode.calcapproach.DataClassWithChangedHashCalc;
 import org.openjdk.jmh.annotations.*;
 
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Function;
-import java.util.stream.Stream;
+import java.util.stream.Collectors;
 
 import static java.util.stream.Collectors.toMap;
 
 @OutputTimeUnit(TimeUnit.MILLISECONDS)
 public class HashCodeCalculationMapBenchmark extends BenchmarkBase {
-	private static final Function<Long, DataClass> LONG_DATA_CLASS_FUNCTION =
-			aLong -> new DataClass(
-					aLong,
-					aLong.toString()
-			);
-	private static final Function<Long, DataClassWithChangedHashCalc> LONG_DATA_CLASS_WITH_CHANGED_HASH_CALC_FUNCTION =
-			aLong -> new DataClassWithChangedHashCalc(
-					aLong,
-					aLong.toString()
-			);
-	
-	private Stream<Long> stream1;
-	private Stream<Long> stream2;
+    private static final Function<Long, DataClass> LONG_DATA_CLASS_FUNCTION =
+            aLong -> new DataClass(
+                    aLong,
+                    aLong.toString()
+            );
+    private static final Function<Long, DataClassWithChangedHashCalc> LONG_DATA_CLASS_WITH_CHANGED_HASH_CALC_FUNCTION =
+            aLong -> new DataClassWithChangedHashCalc(
+                    aLong,
+                    aLong.toString()
+            );
 
-	@Param({"10", "100", "1000"})
-	private int size;
+    private List<Long> longs;
 
-	@Setup
-	public void initTrial() {
-		super.init();
-	}
+    @Param({"10", "100", "1000"})
+    private int size;
 
-	@Setup(value = Level.Invocation)
-	public void init() {
-		stream1 = random.longs(size).boxed();
-		stream2 = random.longs(size).boxed();
-	}
+    @Setup
+    public void initTrial() {
+        super.init();
+        longs = random.longs(size).boxed().collect(Collectors.toList());
+    }
 
-	@Benchmark
-	public Map<DataClass, Long> measureConventionalHashCodeCalculation() {
-		return stream1
-				.collect(toMap(
-						LONG_DATA_CLASS_FUNCTION,
-						Function.identity()
-				));
-	}
+    @Benchmark
+    public Map<DataClass, Long> measureConventionalHashCodeCalculation() {
+        return longs.stream()
+                .collect(toMap(
+                        LONG_DATA_CLASS_FUNCTION,
+                        Function.identity()
+                ));
+    }
 
-	@Benchmark
-	public Map<DataClassWithChangedHashCalc, Long> measureHashCodeCalculationWithObjectsHash() {
-		return stream2
-				.collect(toMap(
-						LONG_DATA_CLASS_WITH_CHANGED_HASH_CALC_FUNCTION,
-						Function.identity()
-				));
-	}
+    @Benchmark
+    public Map<DataClassWithChangedHashCalc, Long> measureHashCodeCalculationWithObjectsHash() {
+        return longs.stream()
+                .collect(toMap(
+                        LONG_DATA_CLASS_WITH_CHANGED_HASH_CALC_FUNCTION,
+                        Function.identity()
+                ));
+    }
 }
