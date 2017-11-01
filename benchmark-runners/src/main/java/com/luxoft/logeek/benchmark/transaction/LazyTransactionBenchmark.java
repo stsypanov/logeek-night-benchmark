1e25691104ffe43dce94c2a62d934c66ca485b7c
stsypanov <STsypanov@luxoft.com>
2017-11-01 16:25:12 +0200

    LazyTransactionBenchmark: use separate class to hold state + simplify setup

--- benchmark-runners/src/main/java/com/luxoft/logeek/benchmark/transaction/LazyTransactionBenchmark.java
+++ benchmark-runners/src/main/java/com/luxoft/logeek/benchmark/transaction/LazyTransactionBenchmark.java
@@ -1,58 +1,48 @@
 package com.luxoft.logeek.benchmark.transaction;
 
-import com.luxoft.logeek.benchmark.ContextAwareBenchmarkBase;
+import com.luxoft.logeek.benchmark.ContextAwareBenchmark;
 import com.luxoft.logeek.dto.CashFlowDto;
 import com.luxoft.logeek.service.ltav.EagerLtavService;
 import com.luxoft.logeek.service.ltav.LazyLtavService;
 import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.infra.Blackhole;
 
 import java.util.concurrent.TimeUnit;
 
+@BenchmarkMode(Mode.AverageTime)
 @OutputTimeUnit(TimeUnit.MICROSECONDS)
-public class LazyTransactionBenchmark extends ContextAwareBenchmarkBase {
-	private CashFlowDto dto;
-	private LazyLtavService lazyService;
-	private EagerLtavService eagerService;
+public class LazyTransactionBenchmark {
 
-	@Setup
-	public void init() {
-		super.init();
-		lazyService = context.getBean(LazyLtavService.class);
-		eagerService = context.getBean(EagerLtavService.class);
-	}
+    @Benchmark
+    public void measureLazyTransaction(Blackhole bh, Data data) {
+        try {
+            bh.consume(data.lazyService.createCashFlow(data.dto));
+        } catch (Exception e) {
+            bh.consume(e);
+        }
+    }
 
-	@Setup(Level.Iteration)
-	public void prepareFreshData() {
-		dto = initDto();
-	}
+    @Benchmark
+    public void measureEagerTransaction(Blackhole bh, Data data) {
+        try {
+            bh.consume(data.eagerService.createCashFlow(data.dto));
+        } catch (Exception e) {
+            bh.consume(e);
+        }
+    }
 
-	@Benchmark
-	public Long measureLazyTransaction() {
-		try {
-			return lazyService.createCashFlow(dto);
-		} catch (RuntimeException e) {
-			return System.currentTimeMillis() >>> 3;
-		}
-	}
+    @State(Scope.Thread)
+    public static class Data extends ContextAwareBenchmark {
+        private CashFlowDto dto;
+        private LazyLtavService lazyService;
+        private EagerLtavService eagerService;
 
-	@Benchmark
-	public Long measureEagerTransaction() {
-		try {
-			return eagerService.createCashFlow(dto);
-		} catch (RuntimeException e) {
-			return System.currentTimeMillis() << 2;
-		}
-	}
-
-	private CashFlowDto initDto() {
-		CashFlowDto dto;
-		for (int i = random.nextInt(); ; i++) {
-			if (i % 2 != 0) {
-				dto = new CashFlowDto(i);
-				break;
-			}
-		}
-
-		return dto;
-	}
+        @Setup
+        public void init() {
+            super.init();
+            lazyService = context.getBean(LazyLtavService.class);
+            eagerService = context.getBean(EagerLtavService.class);
+            dto = new CashFlowDto(3);
+        }
+    }
 }
