bcf177d20956f0aa08845404892bfcabbdec1311
stsypanov <sergei.tsypanov@yandex.ru>
2017-11-18 23:32:08 +0200

    use separate class to hold state

--- benchmark-runners/src/main/java/com/luxoft/logeek/benchmark/bulk/BulkOperationBenchmark.java
+++ benchmark-runners/src/main/java/com/luxoft/logeek/benchmark/bulk/BulkOperationBenchmark.java
@@ -4,100 +4,104 @@ import com.luxoft.logeek.benchmark.BenchmarkBase;
 import org.openjdk.jmh.annotations.*;
 
 import java.util.*;
+import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.TimeUnit;
 
 import static java.util.stream.Collectors.toList;
 
-@Warmup(batchSize = 100, iterations = 100)
-@Measurement(batchSize = 100, iterations = 100)
-@BenchmarkMode(Mode.SingleShotTime)
-@OutputTimeUnit(TimeUnit.MICROSECONDS)
+@SuppressWarnings("ALL")
+@BenchmarkMode(Mode.Throughput)
+@OutputTimeUnit(TimeUnit.MILLISECONDS)
 public class BulkOperationBenchmark extends BenchmarkBase {
-	
-	@Param({"10", "100", "1000", "10000"})
-	private int itemsCount;
-	
-	private List<Long> initialItems;
-	private Long[] initialItemsArray;
 
-	@Setup
-	public void init() {
-		super.init();
-	}
+    @Benchmark
+    public List<Long> measureAddOneByOne(Data data) {
+        List<Long> newList = new ArrayList<>();
+        for (Long item : data.items) {
+            newList.add(item);
+        }
+        return newList;
+    }
 
-	@Setup(Level.Iteration)
-	public void initIteration() {
-		initialItems = random.longs(itemsCount).boxed().collect(toList());
-		initialItemsArray = initialItems.toArray(new Long[initialItems.size()]);
-	}
+    @Benchmark
+    public List<Long> measureAddAll_ArrayList(Data data) {
+        List<Long> newList = new ArrayList<>();
+        newList.addAll(data.items);
+        return newList;
+    }
 
-	@Benchmark
-	public List<Long> measureAddOneByOne_ArrayList() {
-		List<Long> newList = new ArrayList<>();
-		for (Long item : initialItems) {
-			newList.add(item);
-		}
-		return newList;
-	}
+    @Benchmark
+    public List<Long> measureAddAllViaConstructorArg_ArrayList(Data data) {
+        List<Long> newList = new ArrayList<>(data.items);
+        return newList;
+    }
 
-	@Benchmark
-	public List<Long> measureAddAll_ArrayList() {
-		List<Long> newList = new ArrayList<>();
-		newList.addAll(initialItems);
-		return newList;
-	}
+    @Benchmark
+    public Set<Long> measureAddOneByOne_HashSet(Data data) {
+        Set<Long> newSet = new HashSet<>();
+        for (Long item : data.items) {
+            newSet.add(item);
+        }
+        return newSet;
+    }
 
-	@Benchmark
-	public List<Long> measureAddAllViaConstructorArg_ArrayList() {
-		List<Long> newList = new ArrayList<>(initialItems);
-		return newList;
-	}
+    @Benchmark
+    public Set<Long> measureAddAll_HashSet(Data data) {
+        Set<Long> newSet = new HashSet<>();
+        newSet.addAll(data.items);
+        return newSet;
+    }
 
-	@Benchmark
-	public Set<Long> measureAddOneByOne_HashSet() {
-		Set<Long> newSet = new HashSet<>();
-		for (Long item : initialItems) {
-			newSet.add(item);
-		}
-		return newSet;
-	}
+    @Benchmark
+    public Set<Long> measureAddAllViaConstructorArg_HashSet(Data data) {
+        Set<Long> newSet = new HashSet<>(data.items);
+        return newSet;
+    }
 
-	@Benchmark
-	public Set<Long> measureAddAll_HashSet() {
-		Set<Long> newSet = new HashSet<>();
-		newSet.addAll(initialItems);
-		return newSet;
-	}
+    @Benchmark
+    public Set<Long> measureAddAllVarArgViaAsList_HashSet(Data data) {
+        Set<Long> set = new HashSet<>(Arrays.asList(data.array));
+        return set;
+    }
 
-	@Benchmark
-	public Set<Long> measureAddAllViaConstructorArg_HashSet() {
-		Set<Long> newSet = new HashSet<>(initialItems);
-		return newSet;
-	}
+    @Benchmark
+    public Set<Long> measureAddAllVarArgViaCollectionsAddAll_HashSet(Data data) {
+        Set<Long> set = new HashSet<>(data.array.length);
+        Collections.addAll(set, data.array);
+        return set;
+    }
 
-	@Benchmark
-	public Set<Long> measureAddAllVarArgViaAsList_HashSet() {
-		Set<Long> set = new HashSet<>(Arrays.asList(initialItemsArray));
-		return set;
-	}
+    @Benchmark
+    public List<Long> measureAddAllVarArgViaAsList_ArrayList(Data data) {
+        List<Long> set = new ArrayList<>(Arrays.asList(data.array));
+        return set;
+    }
 
-	@Benchmark
-	public Set<Long> measureAddAllVarArgViaCollectionsAddAll_HashSet() {
-		Set<Long> set = new HashSet<>(initialItemsArray.length);
-		Collections.addAll(set, initialItemsArray);
-		return set;
-	}
+    @Benchmark
+    public List<Long> measureAddAllVarArgViaCollectionsAddAll_ArrayList(Data data) {
+        List<Long> set = new ArrayList<>(data.array.length);
+        Collections.addAll(set, data.array);
+        return set;
+    }
 
-	@Benchmark
-	public List<Long> measureAddAllVarArgViaAsList_ArrayList() {
-		List<Long> set = new ArrayList<>(Arrays.asList(initialItemsArray));
-		return set;
-	}
+    @State(Scope.Thread)
+    public static class Data {
+        @Param({"10", "100", "1000"})
+        private int count;
 
-	@Benchmark
-	public List<Long> measureAddAllVarArgViaCollectionsAddAll_ArrayList() {
-		List<Long> set = new ArrayList<>(initialItemsArray.length);
-		Collections.addAll(set, initialItemsArray);
-		return set;
-	}
+        private ThreadLocalRandom random;
+        List<Long> items;
+        Long[] array;
+
+        @Setup
+        public void init() {
+            random = ThreadLocalRandom.current();
+        }
+
+        @Setup(Level.Iteration)
+        public void initIteration() {
+            items = random.longs(count).boxed().collect(toList());
+            array = items.toArray(new Long[0]);
+        }
+    }
 }
