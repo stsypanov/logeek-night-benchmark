ef4b5be8cfd8a618919198506dc5b1780fdd8703
stsypanov <STsypanov@luxoft.com>
2017-11-20 15:43:26 +0200

    use separate class to hold state

--- benchmark-runners/src/main/java/com/luxoft/logeek/benchmark/iterator/IteratorFromStreamBenchmark.java
+++ benchmark-runners/src/main/java/com/luxoft/logeek/benchmark/iterator/IteratorFromStreamBenchmark.java
@@ -1,10 +1,10 @@
 package com.luxoft.logeek.benchmark.iterator;
 
-import com.luxoft.logeek.benchmark.BenchmarkBase;
 import org.openjdk.jmh.annotations.*;
 import org.openjdk.jmh.infra.Blackhole;
 
 import java.util.*;
+import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.TimeUnit;
 
 import static java.util.stream.Collectors.toCollection;
@@ -12,31 +12,13 @@ import static java.util.stream.Collectors.toList;
 
 @BenchmarkMode(Mode.AverageTime)
 @OutputTimeUnit(TimeUnit.NANOSECONDS)
-public class IteratorFromStreamBenchmark extends BenchmarkBase {
-
-    private Collection<Integer> items;
-
-    @Param({"10", "100", "1000"})
-    private int size;
-
-    @Param({"list", "set"})
-    private String collectionType;
-
-    @Setup
-    public void init() {
-        super.init();
-        if ("list".equals(collectionType)) {
-            items = random.ints(size).boxed().collect(toCollection(ArrayList::new));
-        } else {
-            items = random.ints(size).boxed().collect(toCollection(HashSet::new));
-        }
-    }
+public class IteratorFromStreamBenchmark {
 
     @Benchmark
-    public void measureIteratorFromCollectedList(Blackhole bh) {
-        Iterator<String> iterator = items.stream()
+    public void measureIteratorFromCollectedList(Data data, Blackhole bh) {
+        Iterator<String> iterator = data.items.stream()
                 .map(Object::toString)
-                .collect(toList())
+                .collect(toList())//todo add case for toSet()
                 .iterator();
 
         while (iterator.hasNext())
@@ -44,8 +26,8 @@ public class IteratorFromStreamBenchmark extends BenchmarkBase {
     }
 
     @Benchmark
-    public void measureIteratorFromStream(Blackhole bh) {
-        Iterator<String> iterator = items.stream()
+    public void measureIteratorFromStream(Data data, Blackhole bh) {
+        Iterator<String> iterator = data.items.stream()
                 .map(Object::toString)
                 .iterator();
 
@@ -54,9 +36,33 @@ public class IteratorFromStreamBenchmark extends BenchmarkBase {
     }
 
     @Benchmark
-    public void measureForEach(Blackhole bh) {
-        items.stream()
+    public void measureForEach(Data data, Blackhole bh) {
+        data.items.stream()
                 .map(Object::toString)
                 .forEach(bh::consume);
+    }
+
+    @State(Scope.Thread)
+    public static class Data {
+        private Collection<Integer> items;
+
+        @Param({"10", "100", "1000"})
+        private int size;
+
+        @Param({"list", "set"})
+        private String collectionType;
+
+        private ThreadLocalRandom random;
+
+        @Setup
+        public void init() {
+            random = ThreadLocalRandom.current();
+
+            if ("list".equals(collectionType)) {
+                items = random.ints(size).boxed().collect(toCollection(ArrayList::new));
+            } else {
+                items = random.ints(size).boxed().collect(toCollection(HashSet::new));
+            }
+        }
     }
 }
