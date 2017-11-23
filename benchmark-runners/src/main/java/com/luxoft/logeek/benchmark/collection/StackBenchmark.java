82d22e5d6be84c969f35b174924b31d4e2c1902f
stsypanov <STsypanov@luxoft.com>
2017-11-23 17:40:34 +0200

    add StackBenchmark

---
+++ benchmark-runners/src/main/java/com/luxoft/logeek/benchmark/collection/StackBenchmark.java
@@ -0,0 +1,59 @@
+package com.luxoft.logeek.benchmark.collection;
+
+import com.luxoft.logeek.collections.IdeaStack;
+import org.openjdk.jmh.annotations.*;
+
+import java.util.Stack;
+import java.util.concurrent.TimeUnit;
+
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
+@Fork(jvmArgsAppend = {"-XX:+UseParallelGC", "-Xms4g", "-Xmx4g"})
+public class StackBenchmark {
+
+    @Benchmark
+    public Integer measureJdkStackPush(Data data) {
+        return data.jdkStack.push(data.integer);
+    }
+
+    @Benchmark
+    public Integer measureIdeaStackPush(Data data) {
+        return data.ideaStack.push(data.integer);
+    }
+
+    @Benchmark
+    public Integer measureJdkStackPeek(Data data) {
+        return data._jdkStack.peek();
+    }
+
+    @Benchmark
+    public Integer measureIdeaStackPeek(Data data) {
+        return data._ideaStack.peek();
+    }
+
+    @State(Scope.Thread)
+    public static class Data {
+        //stacks to measure peek()
+        Stack<Integer> _jdkStack;
+        IdeaStack<Integer> _ideaStack;
+
+        Stack<Integer> jdkStack;
+        IdeaStack<Integer> ideaStack;
+        Integer integer = 1;
+
+        @Setup
+        public void setup() {
+            _jdkStack = new Stack<>();
+            _jdkStack.push(integer);
+
+            _ideaStack = new IdeaStack<>();
+            _ideaStack.push(integer);
+        }
+
+        @Setup(Level.Iteration)
+        public void setupIteration() {
+            jdkStack = new Stack<>();
+            ideaStack = new IdeaStack<>();
+        }
+    }
+}
---
+++ benchmark-source/src/main/java/com/luxoft/logeek/collections/IdeaStack.java
@@ -0,0 +1,75 @@
+package com.luxoft.logeek.collections;
+
+
+import java.util.*;
+
+/**
+ * A drop-in replacement for {@link java.util.Stack} based on {@link ArrayList} (instead of {@link Vector})
+ * and therefore is (1) not synchronized and (2) faster.
+ *
+ * @author max
+ */
+public class IdeaStack<T> extends ArrayList<T> {
+  public IdeaStack() { }
+
+  public IdeaStack(int initialCapacity) {
+    super(initialCapacity);
+  }
+
+  public IdeaStack( Collection<T> init) {
+    super(init);
+  }
+
+  public IdeaStack( T... items) {
+    for (T item : items) {
+      push(item);
+    }
+  }
+
+  public T push(T t) {
+    add(t);
+    return t;
+  }
+
+  public T peek() {
+    final int size = size();
+    if (size == 0) throw new EmptyStackException();
+    return get(size - 1);
+  }
+
+  public T pop() {
+    final int size = size();
+    if (size == 0) throw new EmptyStackException();
+    return remove(size - 1);
+  }
+
+  public T tryPop() {
+    return isEmpty() ? null : pop();
+  }
+
+  public boolean empty() {
+    return isEmpty();
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (o instanceof RandomAccess && o instanceof List) {
+      List other = (List)o;
+      if (size() != other.size()) {
+        return false;
+      }
+
+      for (int i = 0; i < other.size(); i++) {
+        Object o1 = other.get(i);
+        Object o2 = get(i);
+        if (!(o1 == null ? o2 == null : o1.equals(o2))) {
+          return false;
+        }
+      }
+
+      return true;
+    }
+
+    return super.equals(o);
+  }
+}
