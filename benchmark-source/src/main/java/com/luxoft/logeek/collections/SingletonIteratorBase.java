549842ee493872c4ea9ca04a416a0cec9dd0c54e
stsypanov <sergei.tsypanov@yandex.ru>
2017-11-26 22:13:19 +0200

    add SmartList

---
+++ benchmark-source/src/main/java/com/luxoft/logeek/collections/SingletonIteratorBase.java
@@ -0,0 +1,27 @@
+package com.luxoft.logeek.collections;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public abstract class SingletonIteratorBase<T> implements Iterator<T> {
+  private boolean myVisited;
+
+  @Override
+  public final boolean hasNext() {
+    return !myVisited;
+  }
+
+  @Override
+  public final T next() {
+    if (myVisited) {
+      throw new NoSuchElementException();
+    }
+    myVisited = true;
+    checkCoModification();
+    return getElement();
+  }
+
+  protected abstract void checkCoModification();
+
+  protected abstract T getElement();
+}
