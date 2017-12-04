824f9673863f3de669e738425ec4fa34b20c5a36
stsypanov <STsypanov@luxoft.com>
2017-12-04 13:40:14 +0200

    cosmetic changes into LoopVsStreamBenchmark

--- benchmark-runners/src/main/java/com/luxoft/logeek/benchmark/stream/LoopVsStreamBenchmark.java
+++ benchmark-runners/src/main/java/com/luxoft/logeek/benchmark/stream/LoopVsStreamBenchmark.java
@@ -8,8 +8,10 @@ import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Function;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+
+import static java.util.function.Function.*;
+import static java.util.stream.Collectors.*;
 
 @BenchmarkMode(Mode.AverageTime)
 @OutputTimeUnit(TimeUnit.NANOSECONDS)
@@ -18,22 +20,26 @@ public class LoopVsStreamBenchmark {
 
     @Benchmark
     public Collection<Integer> measureOldMapping(Data data) {
-        return oldMap2List(data.collection, Function.identity());
+        ArrayList<Integer> list = new ArrayList<>(data.collection.size());
+        for (Integer t : data.collection) {
+            list.add(Function.<Integer>identity().apply(t));
+        }
+        return list;
     }
 
     @Benchmark
     public Collection<Integer> measureStream(Data data) {
-        return stream(data.collection, Function.identity());
+        return stream(data.collection, identity());
     }
 
     @Benchmark
     public Collection<Integer> measureSizedStream(Data data) {
-        return sizedStream(data.collection, Function.identity());
+        return sizedStream(data.collection, identity());
     }
 
     @Benchmark
     public Collection<Integer> measureIdeaMapping(Data data) {
-        return map2List(data.collection, Function.identity());
+        return map2List(data.collection, identity());
     }
 
     @State(Scope.Thread)
@@ -45,31 +51,22 @@ public class LoopVsStreamBenchmark {
 
         @Setup
         public void setup() {
-            collection = IntStream.range(0, count).boxed().collect(Collectors.toList());
+            collection = IntStream.range(0, count).boxed().collect(toList());
         }
-    }
-
-
-    private static <T, V> List<V> oldMap2List(Collection<? extends T> collection, Function<T, V> mapper) {
-        List<V> list = new ArrayList<>(collection.size());
-        for (final T t : collection) {
-            list.add(mapper.apply(t));
-        }
-        return list;
     }
 
     private static <T, V> List<V> stream(Collection<? extends T> collection, Function<T, V> mapper) {
-        return collection.stream().map(mapper::apply).collect(Collectors.toList());
+        return collection.stream().map(mapper::apply).collect(toList());
     }
 
     private static <T, V> List<V> sizedStream(Collection<? extends T> collection, Function<T, V> mapper) {
-        return collection.stream().map(mapper::apply).collect(Collectors.toCollection(() -> new ArrayList<>(collection.size())));
+        return collection.stream().map(mapper::apply).collect(toCollection(() -> new ArrayList<>(collection.size())));
     }
 
     private static <T, V> List<V> map2List(Collection<? extends T> collection, Function<T, V> mapper) {
         if (collection.isEmpty()) return Collections.emptyList();
         ArrayList<V> list = new ArrayList<>(collection.size());
-        for (final T t : collection) {
+        for (T t : collection) {
             list.add(mapper.apply(t));
         }
         return list;
