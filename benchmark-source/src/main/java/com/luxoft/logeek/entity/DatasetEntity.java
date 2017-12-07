28a700bd12a4e2d3b19e1a8c43cf51fd2189b797
stsypanov <STsypanov@luxoft.com>
2017-12-07 17:01:16 +0200

    remove unused files

--- benchmark-source/src/main/java/com/luxoft/logeek/entity/DatasetEntity.java
+++
@@ -1,7 +0,0 @@
-package com.luxoft.logeek.entity;
-
-public interface DatasetEntity {
-	String getExtId();
-
-	String getStatus();
-}
--- benchmark-source/src/main/java/com/luxoft/logeek/example/BaseState.java
+++
@@ -1,8 +0,0 @@
-package com.luxoft.logeek.example;
-
-import com.luxoft.logeek.entity.DatasetEntity;
-
-public class BaseState implements State {
-	public BaseState(DatasetEntity ds) {
-	}
-}
