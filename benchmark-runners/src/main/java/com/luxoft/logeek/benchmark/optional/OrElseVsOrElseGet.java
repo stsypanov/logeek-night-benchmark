a2c3213edb6e2e43851a2f0a18007be25f070042
stsypanov <STsypanov@luxoft.com>
2017-12-27 19:08:14 +0200

    simplify OrElseVsOrElseGet

--- benchmark-runners/src/main/java/com/luxoft/logeek/benchmark/optional/OrElseVsOrElseGet.java
+++ benchmark-runners/src/main/java/com/luxoft/logeek/benchmark/optional/OrElseVsOrElseGet.java
@@ -1,7 +1,6 @@
 package com.luxoft.logeek.benchmark.optional;
 
 
-import com.luxoft.logeek.benchmark.ContextAwareBenchmark;
 import org.openjdk.jmh.annotations.*;
 
 import java.util.Optional;
@@ -9,36 +8,25 @@ import java.util.concurrent.TimeUnit;
 
 @BenchmarkMode(Mode.AverageTime)
 @OutputTimeUnit(TimeUnit.NANOSECONDS)
-@Fork(jvmArgsAppend = {"-XX:+UseParallelGC", "-Xms4g", "-Xmx4g"})
 public class OrElseVsOrElseGet {
 
     @Benchmark
-    public String measureOrElse(Data data) {
-        return data.orElse();
+    public String measureOrElse() {
+        return Optional.<String>empty().orElse(get());
     }
 
     @Benchmark
-    public String measureOrElseGet(Data data) {
-        return data.orElseGet();
+    public String measureOrElseGetMethodRef() {
+        return Optional.<String>empty().orElseGet(this::get);
     }
 
+    @Benchmark
+    public String measureOrElseGetLambda() {
+        return  Optional.<String>empty().orElseGet(() -> get());
+    }
 
-    @State(Scope.Thread)
-    @SuppressWarnings("OptionalUsedAsFieldOrParameterType")
-    public static class Data extends ContextAwareBenchmark {
-        Optional<String> emptyOptional = Optional.empty();
-
-        public String orElseGet() {
-            return emptyOptional.orElse(get());
-        }
-
-        public String orElse() {
-            return emptyOptional.orElseGet(this::get);
-        }
-
-        public String get() {
-            return "azaza";
-        }
+    private String get() {
+        return "azaza";
     }
 
 }
