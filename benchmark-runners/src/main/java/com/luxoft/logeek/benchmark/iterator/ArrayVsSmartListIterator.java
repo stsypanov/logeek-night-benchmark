b0b10e782011f6cb9f952b463521b1c1a435faf0
stsypanov <STsypanov@luxoft.com>
2018-01-10 17:44:06 +0200

    add results for enhanced vs original SmartList

--- benchmark-runners/src/main/java/com/luxoft/logeek/benchmark/iterator/ArrayVsSmartListIterator.java
+++ benchmark-runners/src/main/java/com/luxoft/logeek/benchmark/iterator/ArrayVsSmartListIterator.java
@@ -1,11 +1,12 @@
 package com.luxoft.logeek.benchmark.iterator;
 
 import com.intellij.util.containers.SmartList;
+import com.luxoft.logeek.utils.OriginalSmartList;
 import org.openjdk.jmh.annotations.*;
 import org.openjdk.jmh.infra.Blackhole;
 
 import java.util.ArrayList;
-import java.util.Iterator;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -16,34 +17,41 @@ import java.util.stream.IntStream;
 public class ArrayVsSmartListIterator {
 
     @Benchmark
-    public void iteratorFromArrayList(Lists lists, Blackhole bh) {
-        Iterator<Integer> iterator = lists.arrayList.iterator();
-        while (iterator.hasNext()) {
-            bh.consume(iterator.next());
+    public void iterateOverArrayList(Lists lists, Blackhole bh) {
+        for (Integer integer : lists.arrayList) {
+            bh.consume(integer);
         }
     }
 
     @Benchmark
-    public void iteratorFromSmartList(Lists lists, Blackhole bh) {
-        Iterator<Integer> iterator = lists.smartList.iterator();
-        while (iterator.hasNext()) {
-            bh.consume(iterator.next());
+    public void iterateOverEnhancedSmartList(Lists lists, Blackhole bh) {
+        for (Integer integer : lists.smartList) {
+            bh.consume(integer);
         }
     }
 
-    @State(Scope.Thread)
+    @Benchmark
+    public void iterateOverOriginalSmartList(Lists lists, Blackhole bh) {
+        for (Integer integer : lists.originalSmartList) {
+            bh.consume(integer);
+        }
+    }
+
+    @State(Scope.Benchmark)
     public static class Lists {
         ArrayList<Integer> arrayList;
         SmartList<Integer> smartList;
+        OriginalSmartList<Integer> originalSmartList;
 
         @Param({"5", "10", "100", "1000", "10000"})
         int size;
 
         @Setup
         public void setup() {
-            arrayList = IntStream.range(0, size).boxed().collect(Collectors.toCollection(ArrayList::new));
-            smartList = IntStream.range(0, size).boxed().collect(Collectors.toCollection(SmartList::new));
-
+            List<Integer> ints = IntStream.range(0, size).boxed().collect(Collectors.toList());
+            arrayList = new ArrayList<>(ints);
+            smartList = new SmartList<>(ints);
+            originalSmartList = new OriginalSmartList<>(ints);
         }
     }
 }
