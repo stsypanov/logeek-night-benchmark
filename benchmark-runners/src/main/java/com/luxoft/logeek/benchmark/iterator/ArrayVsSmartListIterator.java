4c364b14dd3616389bb4401bfbe40e578050b8f8
stsypanov <STsypanov@luxoft.com>
2018-01-13 12:02:54 +0200

    produce more accurate results

--- benchmark-runners/src/main/java/com/luxoft/logeek/benchmark/iterator/ArrayVsSmartListIterator.java
+++ benchmark-runners/src/main/java/com/luxoft/logeek/benchmark/iterator/ArrayVsSmartListIterator.java
@@ -3,7 +3,6 @@ package com.luxoft.logeek.benchmark.iterator;
 import com.intellij.util.containers.SmartList;
 import com.luxoft.logeek.utils.OriginalSmartList;
 import org.openjdk.jmh.annotations.*;
-import org.openjdk.jmh.infra.Blackhole;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,44 +10,61 @@ import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
+/**
+ * При использовании внутри циклов выражения
+ * bh.consume(Object) растёт погрешность вычисления.
+ * Это же будет наблюдаться при использовании
+ * bh.consume(Integer.intValue())
+ */
 @BenchmarkMode(Mode.AverageTime)
 @OutputTimeUnit(TimeUnit.NANOSECONDS)
 @Fork(jvmArgsAppend = {"-XX:+UseParallelGC", "-Xms4g", "-Xmx4g"})
 public class ArrayVsSmartListIterator {
 
     @Benchmark
-    public void iterateOverArrayList(Lists lists, Blackhole bh) {
+    public int iterateOverArrayList(Lists lists) {
+        int i = 0;
         for (Integer integer : lists.arrayList) {
-            bh.consume(integer);
+            i += integer;
         }
+        return i ;
     }
 
     @Benchmark
-    public void iterateOverEnhancedSmartList(Lists lists, Blackhole bh) {
+    public int iterateOverEnhancedSmartList(Lists lists) {
+        int i = 0;
         for (Integer integer : lists.smartList) {
-            bh.consume(integer);
+            i += integer;
         }
+        return i ;
     }
 
     @Benchmark
-    public void iterateOverOriginalSmartList(Lists lists, Blackhole bh) {
+    public int iterateOverOriginalSmartList(Lists lists) {
+        int i = 0;
         for (Integer integer : lists.originalSmartList) {
-            bh.consume(integer);
+            i += integer;
         }
+        return i ;
     }
 
     @State(Scope.Benchmark)
     public static class Lists {
+        Integer integer = 1;
         ArrayList<Integer> arrayList;
         SmartList<Integer> smartList;
         OriginalSmartList<Integer> originalSmartList;
 
-        @Param({"5", "10", "100", "1000", "10000"})
+        @Param({"1", "5", "10", "100", "1000"})
         int size;
 
         @Setup
         public void setup() {
-            List<Integer> ints = IntStream.range(0, size).boxed().collect(Collectors.toList());
+            List<Integer> ints = IntStream
+                    .range(0, size)
+                    .boxed()
+                    .map(i -> integer)
+                    .collect(Collectors.toList());
             arrayList = new ArrayList<>(ints);
             smartList = new SmartList<>(ints);
             originalSmartList = new OriginalSmartList<>(ints);
