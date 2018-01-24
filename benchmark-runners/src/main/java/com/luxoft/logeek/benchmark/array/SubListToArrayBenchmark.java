74a15f4bb47c559c8c733fdc73585e95b86fae55
stsypanov <sergei.tsypanov@yandex.ru>
2018-01-24 23:09:39 +0200

    add SubListToArrayBenchmark

---
+++ benchmark-runners/src/main/java/com/luxoft/logeek/benchmark/array/SubListToArrayBenchmark.java
@@ -0,0 +1,42 @@
+package com.luxoft.logeek.benchmark.array;
+
+import com.luxoft.logeek.example.ArrayList;
+import org.openjdk.jmh.annotations.*;
+
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static java.util.stream.Collectors.toCollection;
+
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
+@Fork(jvmArgsAppend = {"-XX:+UseParallelGC", "-Xms1g", "-Xmx1g"})
+public class SubListToArrayBenchmark {
+
+    @Benchmark
+    public Integer[] list_typeChecked(Data holder) {
+        return holder.list.toArray(new Integer[0]);
+    }
+
+    @Benchmark
+    public Integer[] subList_typeChecked(Data holder) {
+        return holder.list.subList(0, holder.size).toArray(new Integer[0]);
+    }
+
+    @State(Scope.Thread)
+    public static class Data {
+        ArrayList<Integer> list;
+
+        @Param({"0", "10", "100", "1000"})
+        int size;
+
+        @Setup
+        public void setup() {
+            list = IntStream
+                    .range(0, size)
+                    .boxed()
+                    .collect(toCollection(ArrayList::new));
+        }
+    }
+}
