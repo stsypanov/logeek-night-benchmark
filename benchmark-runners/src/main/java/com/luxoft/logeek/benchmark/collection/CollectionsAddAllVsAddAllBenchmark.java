b5c22294271b38045f81fc0f954e87c0252bb980
stsypanov <STsypanov@luxoft.com>
2018-01-30 15:01:14 +0200

    add CollectionsAddAllVsAddAllBenchmark

---
+++ benchmark-runners/src/main/java/com/luxoft/logeek/benchmark/collection/CollectionsAddAllVsAddAllBenchmark.java
@@ -0,0 +1,71 @@
+package com.luxoft.logeek.benchmark.collection;
+
+import org.openjdk.jmh.annotations.*;
+
+import java.util.*;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.IntStream;
+
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
+@Fork(jvmArgsAppend = {"-XX:+UseParallelGC", "-Xms1g", "-Xmx1g"})
+public class CollectionsAddAllVsAddAllBenchmark {
+
+    @Benchmark
+    public boolean collectionsAddAll(Data data) {
+        Collection<Integer> collection = data.freshCollection();
+        Integer[] array = data.array;
+        return Collections.addAll(collection, array);
+    }
+
+    @Benchmark
+    public boolean addAll(Data data) {
+        Collection<Integer> collection = data.freshCollection();
+        List<Integer> arrayAsList = Arrays.asList(data.array);
+        return collection.addAll(arrayAsList);
+    }
+
+    @State(Scope.Thread)
+    public static class Data {
+        private static final String ARRAY_LIST = "ArrayList";
+        private static final String ARRAY_DEQUE = "ArrayDeque";
+        private static final String HASH_SET = "HashSet";
+        private static final String COPY_ON_WRITE_ARRAY_LIST = "CopyOnWriteArrayList";
+        private static final String CONCURRENT_LINKED_DEQUE = "ConcurrentLinkedDeque";
+
+        Integer array[];
+
+        @Param({ARRAY_LIST, HASH_SET, ARRAY_DEQUE, COPY_ON_WRITE_ARRAY_LIST, CONCURRENT_LINKED_DEQUE})
+        String collection;
+
+        @Param({"0", "10", "100", "1000"})
+        int size;
+
+        @Setup
+        public void setup() {
+            array = IntStream
+                    .range(0, size)
+                    .boxed()
+                    .toArray(Integer[]::new);
+        }
+
+        Collection<Integer> freshCollection() {
+            switch (collection) {
+                case ARRAY_LIST:
+                    return new ArrayList<>();
+                case HASH_SET:
+                    return new HashSet<>();
+                case ARRAY_DEQUE:
+                    return new ArrayDeque<>();
+                case COPY_ON_WRITE_ARRAY_LIST:
+                    return new CopyOnWriteArrayList<>();
+                case CONCURRENT_LINKED_DEQUE:
+                    return new ConcurrentLinkedQueue<>();
+                default:
+                    throw new IllegalArgumentException(collection);
+            }
+        }
+    }
+}
