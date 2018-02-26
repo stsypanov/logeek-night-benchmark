1fff7268a734b9022cb4957431fbadcb4db8be35
stsypanov <sergei.tsypanov@yandex.ru>
2018-02-26 23:15:14 +0200

    configure BenchmarkRunner

--- benchmark-runners/src/main/java/com/luxoft/logeek/benchmark/BenchmarkRunner.java
+++ benchmark-runners/src/main/java/com/luxoft/logeek/benchmark/BenchmarkRunner.java
@@ -1,6 +1,7 @@
 package com.luxoft.logeek.benchmark;
 
-import com.luxoft.logeek.benchmark.bulk.BulkOperationBenchmark;
+import com.luxoft.logeek.benchmark.stream.AllMatchVsContainsAllBenchmark;
+import org.openjdk.jmh.profile.GCProfiler;
 import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.RunnerException;
 import org.openjdk.jmh.runner.options.Options;
@@ -10,15 +11,18 @@ public class BenchmarkRunner {
 
 	public static void main(String[] args) throws RunnerException {
 		Options opt = new OptionsBuilder()
-//				.include(ByteCodeInstrumentationBenchmark.class.getSimpleName())
-//				.include(SimpleDirtyCheckingBenchmark.class.getSimpleName())
-//				.include(SingleEntityFieldModificationBenchmark.class.getSimpleName())
-//				.include(SingleEntityFieldModificationBenchmark.class.getSimpleName())
-//				.include(DistinctVsSetBenchmark.class.getSimpleName())
-				.include(BulkOperationBenchmark.class.getSimpleName())
-//				.warmupIterations(10)
-//				.measurementIterations(10)
-				.forks(4)//0 makes debugging possible
+				.include(AllMatchVsContainsAllBenchmark.class.getSimpleName())
+				.warmupIterations(10)
+				.measurementIterations(10)
+				.forks(5) //0 makes debugging possible
+				.shouldFailOnError(true)
+//				.jvmArgsAppend(
+//						"-XX:+UnlockDiagnosticVMOptions",
+//						"-XX:+PrintCompilation",
+//						"-XX:+PrintInlining",
+//						"-XX:+LogCompilation"
+//				)
+//				.addProfiler(GCProfiler.class)// memory and GC profiler
 				.build();
 
 		new Runner(opt).run();
