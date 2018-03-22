78d26a3c8012afbc3a369976670312b4b19bd603
stsypanov <sergei.tsypanov@yandex.ru>
2018-03-22 21:29:51 +0200

    add one more method to EqualsIgnoreCaseBenchmark

--- benchmark-runners/src/main/java/com/luxoft/logeek/benchmark/string/EqualsIgnoreCaseBenchmark.java
+++ benchmark-runners/src/main/java/com/luxoft/logeek/benchmark/string/EqualsIgnoreCaseBenchmark.java
@@ -22,6 +22,17 @@ public class EqualsIgnoreCaseBenchmark {
         return "true".equalsIgnoreCase(data.str);
     }
 
+    @Benchmark
+    public boolean bestMethod(Data data) {
+        String s = data.str;
+        return s != null
+                && s.length() == 4
+                && (s.charAt(0) | 0x20) == 't'
+                && (s.charAt(1) | 0x20) == 'r'
+                && (s.charAt(2) | 0x20) == 'u'
+                && (s.charAt(3) | 0x20) == 'e';
+    }
+
     @State(Scope.Thread)
     public static class Data {
 
